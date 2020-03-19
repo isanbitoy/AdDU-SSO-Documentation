ce74973795166736fe1638692089411a9c4558ce
isan <isanbitoy@gmail.com>
2020-03-19 20:53:40 +0800

    populate repo

---
+++ .vuepress/config.js
@@ -0,0 +1,72 @@
+module.exports = {
+	title: 'CS272 Dissertation Project',
+	description: 'A single sign-on for AdDU academic information system using java-based authentication platform',
+	themeConfig: {
+		smoothScroll: true,
+		sidebar: [
+			{
+				collapsable: false,
+				sidebarDepth: 0,
+				children: [
+					['/introduction/', 'Introduction']
+				]
+			},
+			{
+				collapsable: false,
+				sidebarDepth: 0,
+				children: [
+					['/tech_app_context/', 'Technology Application Context']
+				]
+			},
+			{
+				collapsable: false,
+				sidebarDepth: 0,
+				children: [
+					['/objective_and_significance/', 'Objective and Significance of the Study']
+				]
+			},
+			{
+				collapsable: false,
+				sidebarDepth: 0,
+				children: [
+					['/scope_and_limitations/', 'Scope and Limitations']
+				]
+			},
+			{
+				collapsable: false,
+				sidebarDepth: 0,
+				children: [
+					['/related_lit_and_tech_application/', 'Review of Related Literature and Technology Applications']
+				]
+			},
+			{
+				collapsable: false,
+				sidebarDepth: 0,
+				children: [
+					['/technology_background/', 'Technology Background']
+				]
+			},
+			{
+				collapsable: false,
+				sidebarDepth: 0,
+				children: [
+					['/project_methodology/', 'Project Methodology']
+				]
+			},
+			{
+				collapsable: false,
+				sidebarDepth: 0,
+				children: [
+					['/results_and_discussion/', 'Results and Discussions']
+				]
+			},
+			{
+				collapsable: false,
+				sidebarDepth: 0,
+				children: [
+					['/conclusion/', 'Conclusion']
+				]
+			}
+		]
+	}
+}
---
+++ introduction/README.md
@@ -0,0 +1,18 @@
+## Introduction
+
+Are you a forgetful person? Do you forget things easily like where you put your car keys, or the password
+for a particular website or social networking site? Did it ever occur to you to use the same username and
+password for a couple, if not all, websites to be able to keep track of your account login details? There aren’t a
+lot of people who know of the technology called Single Sign-On (SSO), which is already being used by big
+companies and some are already using it yet they didn’t know that it was already SSO that they’re using.
+These companies are usually social networking sites, websites that need an authentic email address, and some
+other systems.
+
+Most of these sites, services, and applications require some kind of signing-up applications or a
+registration. You need to prove who you are, so the system knows what resources you have access to. Today,
+you gain access to a site by the means of a username and password. For the user, this means if he/she has
+multiple accounts she might have to type in her username and password at a dozen different sites or more.
+Remembering different passwords on different social networking sites or even sites that you often visit and at
+times you need to sign-on every time you need to access the site is really annoying, now try imagining a
+program that can log you to different sites only providing a few sign-on details from a single sign-on
+application, wouldn’t that make things faster?
